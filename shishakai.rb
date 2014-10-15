cd201876b0991108e84e22b01a1774489a3aafce
Takahiro Yano <speg03@gmail.com>
2014-10-16 04:01:34 +0900

    Update tweeted_pages database

--- shishakai.rb
+++ shishakai.rb
@@ -8,7 +8,8 @@ require 'pg'
 base_uri = "http://movie.walkerplus.com/shisyakai/"
 doc = Nokogiri::HTML(open(base_uri))
 
-movie_info = doc.css("div.previewMovieInfo").map do |info|
+movie_info = {}
+doc.css("div.previewMovieInfo").each do |info|
   anchor = info.css("a").first
   title = anchor.text
   uri = URI.join(base_uri, anchor.attributes["href"].value).to_s
@@ -18,15 +19,23 @@ movie_info = doc.css("div.previewMovieInfo").map do |info|
   date = table_data[1].text
   place = table_data[2].text.sub(/\(.*\)/, '')
 
-  {title: title, uri: uri, limit: limit, date: date, place: place}
+  movie_info[uri] = {title: title, limit: limit, date: date, place: place}
 end
 
-puts movie_info.map {|info| info[:uri] }
-
-puts "----------"
+current_pages = movie_info.keys
 
 db_url = URI.parse(ENV['DATABASE_URL'])
 
 conn = PG.connect({host: db_url.host, port: db_url.port, dbname: db_url.path[1..-1], user: db_url.user, password: db_url.password})
-result = conn.exec("SELECT * FROM tweeted_pages").map {|row| row["uri"] }
-puts result
+previous_pages = conn.exec("SELECT * FROM tweeted_pages").map {|row| row["uri"] }
+
+old_pages = previous_pages - current_pages
+old_pages.each do |page|
+  conn.exec("DELETE FROM tweeted_pages WHERE uri = '#{page}'")
+end
+
+new_pages = current_pages - previous_pages
+new_pages.each do |page|
+  # TODO: tweet movie_info[page]
+  conn.exec("INSERT INTO tweeted_pages VALUES ('#{page}')")
+end
