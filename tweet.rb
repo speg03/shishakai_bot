c5d69f63fd7b147416945232a662ddf53ed24075
Takahiro Yano <speg03@gmail.com>
2014-10-16 02:54:52 +0900

    Add tweet sample

---
+++ tweet.rb
@@ -0,0 +1,12 @@
+#!/usr/bin/env ruby
+
+require 'twitter'
+
+client = Twitter::REST::Client.new do |config|
+  config.consumer_key        = ENV['CONSUMER_KEY']
+  config.consumer_secret     = ENV['CONSUMER_SECRET']
+  config.access_token        = ENV['ACCESS_TOKEN']
+  config.access_token_secret = ENV['ACCESS_TOKEN_SECRET']
+end
+
+client.update("I'm tweeting with gem!!")
