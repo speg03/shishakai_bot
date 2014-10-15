64a3e972d5051dd4d4eff68d250a959df3da3bbc
Takahiro Yano <speg03@gmail.com>
2014-10-16 02:55:07 +0900

    Add sample for getting data from PostgreSQL

--- shishakai.rb
+++ shishakai.rb
@@ -3,11 +3,12 @@
 require 'nokogiri'
 require 'uri'
 require 'open-uri'
+require 'pg'
 
 base_uri = "http://movie.walkerplus.com/shisyakai/"
 doc = Nokogiri::HTML(open(base_uri))
 
-doc.css("div.previewMovieInfo").each do |info|
+movie_info = doc.css("div.previewMovieInfo").map do |info|
   anchor = info.css("a").first
   title = anchor.text
   uri = URI.join(base_uri, anchor.attributes["href"].value)
@@ -17,5 +18,15 @@ doc.css("div.previewMovieInfo").each do |info|
   date = table_data[1].text
   place = table_data[2].text.sub(/\(.*\)/, '')
 
-  puts "title: #{title}, uri: #{uri}, limit: #{limit}, date: #{date}, place: #{place}"
+  {title: title, uri: uri, limit: limit, date: date, place: place}
 end
+
+puts movie_info.map {|info| info[:uri] }
+
+puts "----------"
+
+db_url = URI.parse(ENV['DATABASE_URL'])
+
+conn = PG.connect({host: db_url.host, port: db_url.port, dbname: db_url.path[1..-1], user: db_url.user, password: db_url.password})
+result = conn.exec("SELECT * FROM tweeted_pages").map {|row| row["uri"] }
+puts result
