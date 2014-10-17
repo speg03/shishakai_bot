75f8397381c0bca38812c75b74df0c9069963a97
Takahiro Yano <speg03@gmail.com>
2014-10-17 18:41:09 +0900

    Add test for parsing MovieWalker page.

---
+++ test/unit/test_movie_walker_parse.rb
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+
+require 'test-unit'
+require 'nokogiri'
+require 'open-uri'
+
+require 'shishakai_bot/movie_walker'
+
+module ShishakaiBot
+  class TestMovieWalkerParse < Test::Unit::TestCase
+
+    class <<self
+      def startup
+        @@doc = Nokogiri::HTML(open(MovieWalker::BASE_URI))
+      end
+    end
+
+    test "previewMovieInfo要素が存在していること" do
+      assert { 0 != @@doc.css("div.previewMovieInfo").length }
+    end
+
+    test "previewMovieInfoの各テーブルヘッダが応募締切、開催日時、開催場所であること" do
+      table_header = @@doc.css("div.previewMovieInfo").first.css("th")
+      assert_equal("応募締切", table_header[0].text)
+      assert_equal("開催日時", table_header[1].text)
+      assert_equal("開催場所", table_header[2].text)
+    end
+
+  end
+end
