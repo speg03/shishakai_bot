bf44be767768f0fe02aba1289935d80272ea7eac
Takahiro Yano <speg03@gmail.com>
2014-10-18 22:28:31 +0900

    Add testing for MovieWalker

---
+++ test/unit/test_movie_walker.rb
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+require 'test-unit'
+require 'shishakai_bot/movie_walker'
+
+module ShishakaiBot
+  class TestMovieWalker < Test::Unit::TestCase
+    def setup
+      sample_uri = "https://raw.githubusercontent.com/speg03/shishakai_bot/develop/test/sample/movie_walker.html"
+      @mw = MovieWalker.new(sample_uri)
+    end
+
+    test "3件の試写会ページがあること" do
+      assert { 3 == @mw.pages.length }
+      assert { 3 == @mw.uris.length }
+    end
+
+    data(
+         page1: ["https://raw.githubusercontent.com/shisyakai/8610/", 0],
+         page2: ["https://raw.githubusercontent.com/shisyakai/8607/", 1],
+         page3: ["https://raw.githubusercontent.com/shisyakai/8615/", 2])
+    test "URIがHTML上と逆順でフルパスになっていること" do |data|
+      expected, index = data
+      assert { expected == @mw.uris[index] }
+    end
+  end
+end
